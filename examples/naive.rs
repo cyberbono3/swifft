b68e5c7da0572d7d84a76bff50212cd4cdb5abd4
Andrei Ivasko <cyberbono3@gmail.com>
2025-12-09 13:06:57 +0300

    wip

--- examples/naive.rs
+++ examples/naive.rs
@@ -1,6 +1,6 @@
 use std::fmt::Write;
 
-use swifft::{Block, Key, State, BLOCK_LEN, KEY_LEN};
+use swifft::{pattern::patterned_bytes, Block, Key, State, BLOCK_LEN, KEY_LEN};
 
 fn main() {
     let key = demo_key();
@@ -11,28 +11,16 @@ fn main() {
 }
 
 fn demo_key() -> Key {
-    Key::from(patterned::<KEY_LEN>(11, 7))
+    Key::from(patterned_bytes::<KEY_LEN>(11, 7))
 }
 
 fn demo_block() -> Block {
-    Block::from(patterned::<BLOCK_LEN>(5, 3))
+    Block::from(patterned_bytes::<BLOCK_LEN>(5, 3))
 }
 
 fn compress_and_print(state: &mut State, key: &Key, block: &Block) {
     state.compress(key, block);
     println!("SWIFFT digest (hex): {}", to_hex(state.as_bytes()));
-}
-
-fn patterned<const LEN: usize>(multiplier: u8, addend: u8) -> [u8; LEN] {
-    let mut bytes = [0u8; LEN];
-    fill_pattern(&mut bytes, multiplier, addend);
-    bytes
-}
-
-fn fill_pattern(bytes: &mut [u8], multiplier: u8, addend: u8) {
-    for (i, byte) in bytes.iter_mut().enumerate() {
-        *byte = (i as u8).wrapping_mul(multiplier).wrapping_add(addend);
-    }
 }
 
 fn to_hex(bytes: &[u8]) -> String {
